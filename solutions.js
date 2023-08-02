1d0b767df91f37b13bc476499255885636e1f62a
luisfpeluyera <luis.f.peluyera@gmail.com>
2023-08-01 20:35:42 -0500

    Ten Functions first version

--- solutions.js
+++ solutions.js
@@ -21,3 +21,76 @@
  * concat
  */
 
+// 1.   Define a function named isTrue that takes in any input and returns true if the input provided is exactly equal to true in value and boolean in data type.
+function isTrue(input){
+return input === true;
+};
+
+//2.    Define a function named isFalse that takes in a value and returns true if and only if the argument provided false.
+
+function isFalse(input) {
+    return input === false;
+}
+
+//3.    Define a function named not that takes in any input and returns the boolean opposite of the provided input.
+
+function not(input){
+    return !input;
+
+}
+
+//4.    Define a function named addOne that takes in a single input. If the input is a number or a numeric string, return the value plus one.
+
+function addOne(input){
+    return Number(input) + 1;
+}
+
+
+//5.    Define a function named isEven that takes in a single input. If the input is an even number or a string containing an even number, return true. Any other input should return false for the output.
+
+
+function isEven(input) {
+    if (input === false) {
+        return false;
+    } else {
+
+        return input % 2 == 0;
+
+    }
+}
+
+
+
+
+
+//6. Define a function named isIdentical that takes in two input arguments. If each input is equal both in data type and in value, then return true. If the values are not the same data type or not the same value, return false.
+
+function isIdentical(input1,input2) {
+    return input1 === input2;
+}
+
+//7.    Define a function named isEqual that takes in two input arguments. If each argument is equal only in value, then return true. Otherwise return false.
+
+function isEqual(input1, input2){
+    return input1 == input2;
+}
+
+//8.    Define a function named or that takes in two input arguments. The output returned should be the result of an or operation on both inputs.
+
+function or(input1,input2){
+    return input1 || input2;
+}
+
+//9.    Define a function named and that takes in two input arguments and returns the result of a logical and operation of both inputs
+
+function and(input1, input2){
+    return input1 && input2;
+}
+
+
+//10.   Define a function named concat that takes in two input arguments. If both arguments are strings, then return the concatenated result. If two numbers are provided, then return the string concatenation of each set of numerals.
+
+function concat(input1, input2){
+    return input1.toString() + input2
+}
+
